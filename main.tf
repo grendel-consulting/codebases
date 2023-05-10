5e60dc34be7bb69d1f26e99a685b60e3475cfbdf
James Ramirez <james.ramirez@grendel-consulting.com>
2023-05-10 09:07:53 +0100

    fix: pass thru the visibility flag

--- main.tf
+++ main.tf
@@ -6,9 +6,10 @@ module "repositories" {
     github = github
   }
 
-  name     = each.key
-  tagline  = lookup(each.value, "tagline", null)
-  topics   = lookup(each.value, "topics", [])
-  homepage = lookup(each.value, "homepage", null)
-  pages    = lookup(each.value, "pages", null)
+  name       = each.key
+  tagline    = lookup(each.value, "tagline", null)
+  topics     = lookup(each.value, "topics", [])
+  homepage   = lookup(each.value, "homepage", null)
+  pages      = lookup(each.value, "pages", null)
+  visibility = lookup(each.value, "visibility", "public")
 }
